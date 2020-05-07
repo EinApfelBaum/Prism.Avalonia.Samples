430f988b94730835d76c22569ecd3ee1a52600a3
Folke Baum <Folke.B@mailbox.org>
2020-05-07 09:20:48 +0200

    Add region sample

---
+++ src/02-Regions/Regions/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+using Prism.Ioc;
+using Prism.Unity;
+using Regions.Views;
+
+namespace Regions
+{
+    public class App : PrismApplication
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+            base.Initialize();
+        }
+
+        protected override IAvaloniaObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
