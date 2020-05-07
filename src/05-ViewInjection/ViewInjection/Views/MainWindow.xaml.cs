6d4480a46b12037020f66e6f55c7b05ece649dcf
Folke Baum <Folke.B@mailbox.org>
2020-05-07 09:32:43 +0200

    Add ViewInjection sample

---
+++ src/05-ViewInjection/ViewInjection/Views/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Prism.Ioc;
+using Prism.Regions;
+
+namespace ViewInjection.Views
+{
+    public class MainWindow : Window
+    {
+        private readonly IContainerExtension _container;
+        private readonly IRegionManager _regionManager;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        public MainWindow(IContainerExtension container, IRegionManager regionManager) : this()
+        {
+            _container = container;
+            _regionManager = regionManager;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var view = _container.Resolve<ViewA>();
+            IRegion region = _regionManager.Regions["ContentRegion"];
+            region.Add(view);
+        }
+    }
+}
